cd602354a3378b5aeb2d4664ed16abbba8f7cfaa
Swrangsar Basumatary <swrangsar@aol.com>
2012-10-29 23:21:57 +0530

    added plots

--- findDOAfromULAData.m
+++ findDOAfromULAData.m
@@ -6,7 +6,7 @@ addpath /Users/swrangsarbasumatary/Desktop/advSignalProcAsgn4/
 plotSpectraOfUlaData(15, 32);
 plotSpectraOfUlaData(15, 64);
 plotSpectraOfUlaData(7.5, 64);
-plotSpectraOfUlaData(7.5, 256);
+plotSpectraOfUlaData(7.5, 512);
 
 
 rmpath /Users/swrangsarbasumatary/Desktop/advSignalProcAsgn4/
